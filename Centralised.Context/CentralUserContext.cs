b46778a46eae7a4097fb766852bab0d93390869a
deepakkhemchandani27 <deepakkhemchandani27@gmail.com>
2021-01-17 14:17:15 +0530

    Add project files.

---
+++ Centralised.Context/CentralUserContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Centralised.Context
+{
+    public class CentralUserContext : IdentityDbContext<AppUser>
+    {
+        public CentralUserContext(DbContextOptions<CentralUserContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
